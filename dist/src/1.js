5418142f31b5da990d788fe724cfde4421af4c32
valentine195 <38669521+valentine195@users.noreply.github.com>
2021-12-08 11:30:05 -0500

    removed dist

--- dist/src/1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.result = void 0;
-var inputs_1 = require("../src/inputs");
-var result1 = inputs_1.depths.reduce(function (acc, curr, i, a) { return (i != 0 && a[i] > a[i - 1] ? acc + 1 : acc); }, 0);
-var chunked = inputs_1.depths.map(function (v, i, arr) {
-    return arr.slice(i, i + 3).reduce(function (a, b) { return a + b; });
-});
-exports.result = chunked.reduce(function (acc, curr, i, a) { return (i != 0 && a[i] > a[i - 1] ? acc + 1 : acc); }, 0);
