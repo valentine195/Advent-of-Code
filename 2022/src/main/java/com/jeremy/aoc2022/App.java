9de003a3e0d67d264b4a5cf24bded15e351a4868
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2022-12-02 11:59:25 -0500

    move download to loader, actually save file

--- 2022/src/main/java/com/jeremy/aoc2022/App.java
+++ 2022/src/main/java/com/jeremy/aoc2022/App.java
@@ -2,16 +2,13 @@ package com.jeremy.aoc2022;
 
 import java.util.Calendar;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
 /**
  * Hello world!
  *
  */
 public class App {
     static Runner runner = new Runner();
+    static Loader loader = new Loader();
 
     public static void main(String[] arg) {
         int day;
@@ -20,7 +17,7 @@ public class App {
         } else {
             day = Integer.parseInt(arg[0]);
         }
-        String input = downloadDay(day);
+        String input = loader.getDataForDay(day);
         runner.runDay(day, input);
     }
 
@@ -30,22 +27,6 @@ public class App {
             return true;
         } catch (NumberFormatException e) {
             return false;
-        }
-    }
-
-    public static String downloadDay(int day) {
-        try {
-            OkHttpClient client = new OkHttpClient().newBuilder()
-                    .build();
-            Request request = new Request.Builder()
-                    .url("https://adventofcode.com/2022/day/" + day + "/input")
-                    .addHeader("Cookie",
-                            "session=" + System.getenv("SESSION_COOKIE"))
-                    .build();
-            Response response = client.newCall(request).execute();
-            return response.body().string();
-        } catch (Exception e) {
-            return null;
         }
     }
 }
--- 2022/src/main/java/com/jeremy/aoc2022/Loader.java
+++ 2022/src/main/java/com/jeremy/aoc2022/Loader.java
@@ -1,24 +1,45 @@
 package com.jeremy.aoc2022;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okio.BufferedSink;
+import okio.Okio;
+
 public class Loader {
 
-    public String load(int day) {
-        String path = "day" + day + "/input.txt";
+    public String getDataForDay(int day) {
+        String data = loadInput(day);
+        if (data == null) {
+            data = downloadDay(day);
+        }
+        return data;
+    }
+
+    private String loadData(int day, String type) {
+        String path = "day" + day + "/" + type + ".txt";
         ClassLoader classLoader = getClass().getClassLoader();
         InputStream inputStream = classLoader.getResourceAsStream(path);
         try {
             String data = readFromInputStream(inputStream);
-            System.out.println(data);
             return data;
         } catch (Exception e) {
             return null;
         }
+    }
 
+    public String loadInput(int day) {
+        return loadData(day, "input");
+    }
+
+    public String loadTest(int day) {
+        return loadData(day, "test");
     }
 
     private String readFromInputStream(InputStream inputStream)
@@ -31,5 +52,30 @@ public class Loader {
             }
         }
         return resultStringBuilder.toString();
+    }
+
+    public static String downloadDay(int day) {
+        try {
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            Request request = new Request.Builder()
+                    .url("https://adventofcode.com/2022/day/" + day + "/input")
+                    .addHeader("Cookie",
+                            "session=" + System.getenv("SESSION_COOKIE"))
+                    .addHeader("User-Agent", "github.com/valentine195/advent-of-code by valentine.195@gmail.com")
+
+                    .build();
+            Response response = client.newCall(request).execute();
+            String contents = response.peekBody(Long.MAX_VALUE).string();
+
+            File downloadedFile = new File(System.getenv("RESOURCE_PATH") + "/day" + day, "input.txt");
+            BufferedSink sink = Okio.buffer(Okio.sink(downloadedFile));
+            sink.writeAll(response.body().source());
+            sink.close();
+
+            return contents;
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
