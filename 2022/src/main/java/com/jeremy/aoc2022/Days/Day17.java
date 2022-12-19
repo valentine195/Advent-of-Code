b6c552a7b1d16adaf91db2109bd5fa44f713ad43
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2022-12-19 13:12:30 -0500

    day 17

--- 2022/src/main/java/com/jeremy/aoc2022/Days/Day17.java
+++ 2022/src/main/java/com/jeremy/aoc2022/Days/Day17.java
@@ -3,49 +3,32 @@ package com.jeremy.aoc2022.Days;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.jeremy.aoc2022.Day;
 import com.jeremy.aoc2022.utils.Coord;
 
-enum Jets {
-    Left("<"),
-    Right(">");
-
-    public final String label;
-
-    public String getLabel() {
-        return label;
-    }
-
-    public Coord getCoord() {
-        if (this == Jets.Right) {
-            return Coord.RIGHT;
-        }
-        return Coord.LEFT;
-    }
-
-    private Jets(String label) {
-        this.label = label;
-    }
-
-    static Jets from(String jet) {
-        if (Jets.Right.getLabel().equals(jet)) {
-            return Jets.Right;
-        }
-        return Jets.Left;
-    }
-}
-
 class Shape {
     List<Coord> coords;
 
+    Shape() {
+        this.coords = new ArrayList<>();
+    }
+
     Shape(List<Coord> coords) {
         this.coords = coords;
+    }
+
+    long getTop() {
+        return Collections.max(coords.stream().map(c -> c.y).collect(Collectors.toList()));
     }
 
     List<Coord> sumSelf(Coord coord) {
@@ -57,58 +40,46 @@ class Shape {
 
     static Shape getShape(int index) {
         List<Coord> coords = new ArrayList<>();
-        if (index + 5 % 5 == 0) {
-            coords.add(new Coord(0, 0));
-            coords.add(new Coord(1, 0));
-            coords.add(new Coord(2, 0));
-            coords.add(new Coord(3, 0));
-        }
-        if (index + 6 % 5 == 0) {
-            coords.add(new Coord(1, 0));
-            coords.add(new Coord(0, 1));
-            coords.add(new Coord(2, 1));
-            coords.add(new Coord(1, 2));
-        }
-        if (index + 7 % 5 == 0) {
-            coords.add(new Coord(0, 0));
-            coords.add(new Coord(1, 0));
-            coords.add(new Coord(2, 0));
-            coords.add(new Coord(2, 1));
-            coords.add(new Coord(2, 2));
-        }
-        if (index + 8 % 5 == 0) {
-            coords.add(new Coord(0, 0));
-            coords.add(new Coord(1, 0));
-            coords.add(new Coord(0, 1));
-            coords.add(new Coord(1, 1));
-
-        }
-        if (index + 9 % 5 == 0) {
-            coords.add(new Coord(0, 0));
-            coords.add(new Coord(0, 1));
-            coords.add(new Coord(0, 2));
-            coords.add(new Coord(0, 3));
+        switch (index) {
+            case 0: {
+                coords.add(new Coord(0, 0));
+                coords.add(new Coord(1, 0));
+                coords.add(new Coord(2, 0));
+                coords.add(new Coord(3, 0));
+                break;
+            }
+            case 1: {
+                coords.add(new Coord(1, 0));
+                coords.add(new Coord(0, 1));
+                coords.add(new Coord(1, 1));
+                coords.add(new Coord(2, 1));
+                coords.add(new Coord(1, 2));
+                break;
+            }
+            case 2: {
+                coords.add(new Coord(0, 0));
+                coords.add(new Coord(1, 0));
+                coords.add(new Coord(2, 0));
+                coords.add(new Coord(2, 1));
+                coords.add(new Coord(2, 2));
+                break;
+            }
+            case 3: {
+                coords.add(new Coord(0, 0));
+                coords.add(new Coord(0, 1));
+                coords.add(new Coord(0, 2));
+                coords.add(new Coord(0, 3));
+                break;
+            }
+            case 4: {
+                coords.add(new Coord(0, 0));
+                coords.add(new Coord(1, 0));
+                coords.add(new Coord(0, 1));
+                coords.add(new Coord(1, 1));
+                break;
+            }
         }
         return new Shape(coords);
-    }
-}
-
-class AirJets {
-
-    private final List<String> jets;
-    private int index = 0;
-
-    AirJets(String input) {
-        jets = List.of(input.split(""));
-    }
-
-    public Jets next() {
-        if (index >= jets.size()) {
-            index = 0;
-        }
-        String jet = this.jets.get(index);
-        index++;
-        return Jets.from(jet);
     }
 }
 
@@ -129,61 +100,136 @@ public class Day17 extends Day {
         System.out.println(runPart2() + " time: " + Duration.between(start, Instant.now()));
     }
 
-    Set<Coord> tower = new HashSet<>();
-    HashMap<Integer, Shape> cache = new HashMap<>();
-
-    boolean empty(Coord pos) {
-        return pos.x >= 0 && pos.x <= 6 && pos.y > 0 && !tower.contains((Coord) pos);
+    boolean isEmpty(Coord pos) {
+        if (pos.x < 0 || pos.x > 6)
+            return false;
+        if (pos.y <= 0)
+            return false;
+        return !tower.contains(pos);
     }
 
-    boolean check(Coord pos, Coord dir, Shape rock) { // all(empty(pos+dir+r) for r in rock)
-        return rock.coords.stream().allMatch(coord -> empty(coord.sum(pos, dir)));
+    boolean checkCanMove(Shape rock, Coord dir) {
+        return rock.coords.stream().allMatch(coord -> isEmpty(coord.sum(dir)));
     }
 
-    @Override
-    public String runPart1() {
-        // last four rows
-        AirJets jets = new AirJets(INPUT);
+    Set<Coord> tower;
+    HashMap<Long, HashMap<Integer, Coord>> cache;
+
+    long execute(long iterations) {
+        tower = new HashSet<>();
+        cache = new HashMap<>();
+        List<Integer> jets = INPUT.trim().chars().boxed().map(c -> (c - 61)).collect(Collectors.toList());
+        int jet = 0;
         Coord down = new Coord(0, -1);
-        int index = 0;
-        int top = 0;
-        for (int step = 0; step < 3; step++) {
-            Coord start = new Coord(2, top + 4);
-            Shape shape = Shape.getShape(index);
-            index++;
-
-            while (true) {
-                Coord jet = jets.next().getCoord();
-                if (check(start, jet, shape)) {
-                    start.sumSelf(jet);
+        long index = 0;
+        long top = 0;
+        // can't exit on first cycle because the floor changes things
+        for (int i = 0; i < iterations; i++) {
+            Shape shape = Shape.getShape((int) index);
+            shape.sumSelf(new Coord(2, top + 4));
+            cache.putIfAbsent(index, new HashMap<>());
+            // because the inputs repeat, it should be cyclical...
+            // detect a cycle and exit early
+            // cache the shape index and jet index
+            // Shape -> Jet -> (Iteration, Top at that iteration)
+            if (cache.get(index).containsKey(jet)) {
+                Coord cached = cache.get(index).get(jet);
+                // cached.x is the iteration we last saw this combo at
+                // cached.y is the top level at that iteration
+                // remaining iterations % iterations between now and cached iteration should be
+                // zero for a full cycle
+                long completed = (iterations - i) % (i - cached.x);
+                // check if we've completed a cycle
+                if (completed == 0) {
+                    // this is the remaining cycles.
+                    long remainingCycles = (iterations - i) / (i - cached.x);
+                    //top - cached.y is the height per cycle
+                    return (top + (top - cached.y) * remainingCycles);
                 }
-                if (check(start, down, shape)) {
-                    start.sumSelf(down);
+            } else {
+                cache.get(index).put(jet, new Coord(i, top));
+            }
+            while (true) {
+                Coord dir = new Coord(jets.get(jet), 0);
+                jet = (jet + 1) % jets.size();
+                if (checkCanMove(shape, dir)) {
+                    shape.sumSelf(dir);
+                }
+                if (checkCanMove(shape, down)) {
+                    shape.sumSelf(down);
                 } else {
                     break;
                 }
             }
-            Set<Coord> union = new HashSet<>(shape.sumSelf(start));
-            tower.addAll(union);
-            top = Math.max(top, start.y + new int[] { 1, 0, 2, 2, 3 }[index]);
+            tower.addAll(shape.coords);
+            top = Math.max(top, shape.getTop());
+            index = (index + 1) % 5;
         }
-        return "";
+        return top;
     }
 
-    void addRow(HashMap<Integer, HashMap<Integer, Boolean>> grid, int index) {
-        addRow(grid, index, false);
-    }
-
-    void addRow(HashMap<Integer, HashMap<Integer, Boolean>> grid, int index, boolean value) {
-        grid.put(index, new HashMap<>());
-        for (int j = 0; j < 7; j++) {
-            grid.get(index).put(j, value);
-        }
+    @Override
+    public String runPart1() {
+        return "" + execute(2022);
     }
 
     @Override
     public String runPart2() {
-        return "";
+        return "" + execute((long) 1e12);
+    }
+
+    void drawShape(Shape shape) {
+        long y = Collections.max(shape.coords.stream().map(c -> c.y).collect(Collectors.toList()));
+        long x = Collections.max(shape.coords.stream().map(c -> c.x).collect(Collectors.toList()));
+        Set<Coord> coords = new HashSet<>(shape.coords);
+        for (long row = y; row >= 0; row--) {
+            String rowString = "";
+            for (int col = 0; col <= x; col++) {
+                if (coords.contains(new Coord(col, row))) {
+                    rowString += "#";
+                } else {
+                    rowString += ".";
+                }
+            }
+            System.out.println(rowString);
+        }
+        System.out.println("");
+    }
+
+    void drawTower(int top) {
+        drawTower(top, new Shape());
+    }
+
+    void clear() {
+        /*
+         * System.out.print("\033[H\033[2J");
+         * System.out.flush();
+         */
+        try {
+            Thread.sleep(100);
+        } catch (Exception e) {
+
+        }
+    }
+
+    void drawTower(int top, Shape shape) {
+        clear();
+        for (int i = top; i > 0; i--) {
+            String row = "|";
+            for (int col = 0; col < 7; col++) {
+                Coord coord = new Coord(col, i);
+                if (tower.contains(coord)) {
+                    row += "#";
+                } else if (shape.coords.contains(coord)) {
+                    row += "@";
+                } else {
+                    row += ".";
+                }
+            }
+            row += "|";
+            System.out.println(row);
+        }
+        System.out.println("+-------+\n");
     }
 
 }
