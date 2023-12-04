d9348c2d4b0c456c5b3c96cc8c02630b9ada8d94
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2023-12-03 20:22:29 -0500

    day 3 done

--- 2023/src/puzzles/day_03.rs
+++ 2023/src/puzzles/day_03.rs
@@ -3,13 +3,14 @@ use crate::input;
 pub fn run() {
     let input: Vec<String> = input::read_day_input(3);
 
-    let sum = find_values(&input);
+    let mut sum = find_values(&input, true);
 
     println!("** Part 1 Final: {:?}", sum);
-    println!("** Part 2 Final: {:?}", 0);
+    sum = find_values(&input, false);
+    println!("** Part 2 Final: {:?}", sum);
 }
 
-fn find_values(schematic: &Vec<String>) -> u32 {
+fn find_values(schematic: &Vec<String>, part1: bool) -> u32 {
     let mut prev: Option<&String> = None;
     let mut sum = 0;
     let mut lines = schematic.iter().peekable();
@@ -23,15 +24,20 @@ fn find_values(schematic: &Vec<String>) -> u32 {
             Some(line) => Some(*line),
             _ => None,
         };
-        if prev == None {
-            let lines = vec![curr, next.unwrap()];
-            sum += parse_lines(lines, true);
-        } else if next == None {
-            let lines = vec![prev.unwrap(), curr];
-            sum += parse_lines(lines, false);
+
+        let mut lines: Vec<&String> = Vec::new();
+        if !prev.is_none() {
+            lines.push(prev.unwrap());
+        }
+        lines.push(curr);
+        if !next.is_none() {
+            lines.push(next.unwrap());
+        }
+
+        if part1 {
+            sum += parse_lines(lines, prev.is_none());
         } else {
-            let lines = vec![prev.unwrap(), curr, next.unwrap()];
-            sum += parse_lines(lines, false);
+            sum += parse_gears(lines, prev.is_none());
         }
 
         prev = Some(curr);
@@ -43,18 +49,18 @@ fn parse_lines(lines: Vec<&String>, first: bool) -> u32 {
     let mut sum = 0;
     let mut num = 0;
     let mut is_valid = false;
-    let mut char_vec: Vec<Vec<char>> = Vec::new();
+    let mut vec: Vec<Vec<char>> = Vec::new();
     for line in lines {
-        char_vec.push(line.chars().collect());
+        vec.push(line.chars().collect());
     }
-    let chars = if first { &char_vec[0] } else { &char_vec[1] };
+    let chars = if first { &vec[0] } else { &vec[1] };
     for i in 0..chars.len() {
         if chars[i].is_digit(10) {
             // Add the digit to the number
             num = num * 10 + chars[i].to_digit(10).expect("is digit");
 
             for &x in &[-1, 0, 1] {
-                for chars in &char_vec {
+                for chars in &vec {
                     if (i as isize + x).is_positive() && ((i as isize + x) as usize) < chars.len() {
                         let index = (i as isize + x) as usize;
                         if !chars[index].is_digit(10) && chars[index] != '.' {
@@ -76,6 +82,89 @@ fn parse_lines(lines: Vec<&String>, first: bool) -> u32 {
     }
     sum
 }
+
+type Point = (usize, i32);
+fn get_offsets() -> Vec<Point> {
+    //start rows at 0 to index a vec
+    vec![
+        (0, -1),
+        (0, 0),
+        (0, 1),
+        (1, -1),
+        (1, 1),
+        (2, -1),
+        (2, 0),
+        (2, 1),
+    ]
+}
+
+fn parse_gears(lines: Vec<&String>, first: bool) -> u32 {
+    let mut vec: Vec<Vec<char>> = Vec::new();
+    for line in lines {
+        vec.push(line.chars().collect());
+    }
+    let chars = if first { &vec[0] } else { &vec[1] };
+    let offsets = get_offsets();
+    let mut idx = 0;
+    let mut sum = 0;
+    for char in chars {
+        let mut indices: Vec<(isize, isize)> = Vec::new();
+        if *char == '*' {
+            //indices of x,y coords
+            //y -1..1
+            for offset in &offsets {
+                let row = offset.0;
+                if row > vec.len() {
+                    continue;
+                }
+
+                let col = idx + offset.1;
+                let offset_chars = &vec[row];
+
+                if col >= 0
+                    && col < offset_chars.len() as i32
+                    && offset_chars[col as usize].is_numeric()
+                    && (offset.1 == -1
+                        || (offset.1 >= 0 && !offset_chars[(col - 1) as usize].is_numeric()))
+                {
+                    //this is a new number;
+                    indices.push((row as isize, col as isize));
+                    if indices.len() > 2 {
+                        break;
+                    }
+                }
+            }
+        }
+
+        idx += 1;
+        if indices.len() != 2 {
+            continue;
+        }
+        let mut num: u32 = 1;
+        for index in indices {
+            let mut left = index.1;
+            let mut right = index.1;
+            let chars = &vec[index.0 as usize];
+            while left > 0 && chars[(left - 1) as usize].is_numeric() {
+                left = left - 1;
+            }
+            while right < (chars.len() - 1) as isize && chars[(right + 1) as usize].is_numeric() {
+                right = right + 1;
+            }
+            let slice = chars[(left as usize)..=(right as usize)]
+                .to_vec()
+                .iter()
+                .collect::<String>()
+                .parse::<u32>()
+                .unwrap();
+            num *= slice;
+        }
+        sum += num;
+    }
+
+    sum
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -95,6 +184,7 @@ mod tests {
 .664.598..",
         );
 
-        assert_eq!(4361, find_values(&input))
+        assert_eq!(4361, find_values(&input, true));
+        assert_eq!(467835, find_values(&input, false))
     }
 }
