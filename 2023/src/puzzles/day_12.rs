e64b44bf093127d43c0fd05d212a43ab51abacd8
Jeremy Valentine <valej10@nationwide.com>
2023-12-13 11:47:05 -0500

    DAY 12!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- 2023/src/puzzles/day_12.rs
+++ 2023/src/puzzles/day_12.rs
@@ -1,121 +1,170 @@
-use std::{collections::HashMap, fmt::Error, str::FromStr};
+use std::{collections::HashMap, fmt::Error};
 
 use crate::input;
 
 pub fn run() {
     let input: Vec<String> = input::read_day_input(12);
-
-    println!("** Part 1 Final: {:?}", 0);
-    println!("** Part 2 Final: {:?}", 0);
+    if input.is_empty() {
+        panic!("No input read")
+    }
+    println!("** Part 1 Final: {:?}", Field::parse(&input, 1).calculate());
+    println!("** Part 2 Final: {:?}", Field::parse(&input, 5).calculate());
 }
+
+struct Field {
+    springs: Vec<SpringRow>,
+}
+impl Field {
+    fn parse(input: &Vec<String>, fold: usize) -> Self {
+        let mut vec = Vec::new();
+
+        for line in input {
+            vec.push(SpringRow::parse(line, fold).unwrap());
+        }
+
+        Field { springs: vec }
+    }
+    fn calculate(&mut self) -> u64 {
+        let mut sum = 0;
+
+        for spring in self.springs.iter_mut() {
+            sum += spring.get_ways(spring.row.clone(), 0);
+        }
+
+        sum
+    }
+}
+
+#[derive(Debug)]
 struct SpringRow {
-    cache: HashMap<(usize, usize), i32>,
-    row: Vec<char>,
+    cache: HashMap<(String, usize), u64>,
+    row: String,
     groups: Vec<usize>,
 }
 
 impl SpringRow {
-    fn get_ways(&mut self, position: usize, block: usize, adder: Option<char>) -> i32 {
-        /* if self.cache.contains_key(&(position, block)) {
-            return *self.cache.get(&(position, block)).unwrap();
-        } */
-        let mut line = self.row[position..].to_vec();
-        if adder.is_some() {
-            line[0] = adder.unwrap();
-            println!("line: {:?}, adder: {:?}", line, adder.unwrap());
-        }
-        println!(
-            "{:?}",
-            self.row
-                .iter()
-                .map(|c| c.to_string())
-                .collect::<Vec<String>>()
-                .join("")
-        );
-        let mut pointer = " ".repeat(position);
-        pointer.push('^');
-        println!("{:?}", pointer);
-        let groups = &self.groups[block..];
-        println!("line: {}, groups: {}", line.len(), groups.len());
-        let mut val: i32 = 0;
-        if line.is_empty() && groups.is_empty() {
-            //both consumed, found a good one
-            println!("here!!");
-            val = 1;
-        } else if groups.is_empty() && !line.contains(&'#') {
-            //groups done but the remaining spaces can be good springs
-            println!("here!!");
-            val = 1;
-        } else if line.len() < groups.iter().sum::<usize>() + groups.len() - 1 {
-            //line is not long enough for remaining runs
-            val = 0;
-        } else {
-            println!("char: {}", line[0]);
-            val = match line[0] {
-                '.' => self.get_ways(position + 1, block, None),
-                '#' => {
-                    if line[0..groups[0]].contains(&'.') {
-                        //the group range contains a good spring and therefore cannot contain a bad one
-                        0
-                    } else if groups[0] < line.len() && line[groups[0] as usize] == '#' {
-                        0
-                    } else {
-                        println!("here");
-                        self.get_ways(position + block + 1, block + 1, None)
-                    }
-                }
-                '?' => {
-                    self.get_ways(position, block, Some('#'))
-                        + self.get_ways(position, block, Some('.'))
-                }
-                _ => panic!("help"),
-            };
-        }
-
-        self.cache.insert((position, block), val);
-        println!("75 val: {val}");
-        val
-    }
-}
-
-impl FromStr for SpringRow {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+    fn parse(s: &str, fold: usize) -> Result<Self, Error> {
         let [row, groups] = s.split_whitespace().collect::<Vec<&str>>()[..] else {
             return Err(Error);
         };
+        let row = vec![row].repeat(fold).join("?");
 
         let groups = groups
             .split(",")
             .map(|k| k.parse::<usize>().unwrap())
-            .collect::<Vec<usize>>();
+            .collect::<Vec<usize>>()
+            .repeat(fold);
 
         Ok(SpringRow {
             cache: HashMap::new(),
-            row: row.to_string().chars().collect(),
+            row,
             groups,
         })
     }
-}
+    fn get_ways(&mut self, line: String, block: usize) -> u64 {
+        if self.cache.contains_key(&(line.clone(), block)) {
+            return *self.cache.get(&(line, block)).unwrap();
+        }
+        let groups = &self.groups[block..];
 
-fn count_ways(input: &Vec<String>) -> i32 {
-    let mut arrangements = 0;
+        if line.is_empty() {
+            //consumed the line. if there are no more groups to check then we're good.
+            return if groups.is_empty() { 1 } else { 0 };
+        }
 
-    for line in input {
-        let spring_row = line.parse::<SpringRow>().expect("good");
+        if groups.is_empty() && line.contains('#') {
+            //no groups left but there are still broken springs...
+            return 0;
+        }
+        let perms = match line.chars().nth(0).unwrap() {
+            //good spring, move on
+            '.' => self.get_ways(line[1..].to_string(), block),
+            '?' => {
+                //check both options
+                self.get_ways(format!("#{}", line[1..].to_string()), block)
+                    + self.get_ways(format!(".{}", line[1..].to_string()), block)
+            }
+            '#' => {
+                //group to check
+                let group = groups[0];
+                if group < line.len() + 1 && line[0..group].chars().all(|c| c != '.') {
+                    //the group can fit in the remaining line
+                    if group == line.len() {
+                        //group will consume the rest of the line..
+                        if groups[1..].is_empty() {
+                            //no more groups to check, so this is a valid config
+                            1
+                        } else {
+                            //there are more groups to check but we're out of space
+                            0
+                        }
+                    } else {
+                        //this character is the character immediately *following* the group
+                        //line = ###.?.?### etc, group = 3
+                        //###.
+                        //...^
+                        //this *must* be a good spring for it to be a valid config.
+                        match line.chars().nth(group).unwrap_or('X') {
+                            '?' => {
+                                //in order for this to be a valid config, this *has* to be a good spring
+                                self.get_ways(
+                                    format!(".{}", line[group + 1..].to_string()),
+                                    block + 1,
+                                )
+                            }
+                            '.' => {
+                                //consumed a good group of springs
+                                self.get_ways(line[group + 1..].to_string(), block + 1)
+                            }
+                            _ => 0,
+                        }
+                    }
+                } else {
+                    //group cannot fit in this space
+                    0
+                }
+            }
+            _ => panic!("invalid character"),
+        };
+        self.cache.insert((line, block), perms);
+        return perms;
     }
-
-    arrangements
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
+    fn one() {
+        let mut spring = SpringRow::parse(&"???.### 1,1,3", 1).unwrap();
+        assert_eq!(1, spring.get_ways(spring.row.clone(), 0));
+    }
+    #[test]
+    fn two() {
+        let mut spring = SpringRow::parse(&".??..??...?##. 1,1,3", 1).unwrap();
+        assert_eq!(4, spring.get_ways(spring.row.clone(), 0));
+    }
+
+    #[test]
+    fn part_two() {
+        let mut spring = SpringRow::parse(&".??..??...?##. 1,1,3", 5).unwrap();
+        println!("{:?}", spring);
+        assert_eq!(16384, spring.get_ways(spring.row.clone(), 0));
+    }
+
+    #[test]
     fn test() {
-        let input: Vec<String> = input::split_string(".??..??...?##. 1,1,3");
-        let mut spring = input[0].parse::<SpringRow>().unwrap();
-        println!("{}", spring.get_ways(0, 0, None));
+        let input: Vec<String> = input::split_string(
+            "???.### 1,1,3
+            .??..??...?##. 1,1,3
+            ?#?#?#?#?#?#?#? 1,3,1,6
+            ????.#...#... 4,1,1
+            ????.######..#####. 1,6,5
+            ?###???????? 3,2,1",
+        );
+        let mut field = Field::parse(&input, 1);
+
+        assert_eq!(21, field.calculate());
     }
 }
