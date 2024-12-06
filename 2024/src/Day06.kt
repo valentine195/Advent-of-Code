571d0241379af301a1da55a45b2a42c10722babe
Jeremy Valentine <valej10@nationwide.com>
2024-12-06 12:12:00 -0500

    feat: Day 06

---
+++ 2024/src/Day06.kt
@@ -0,0 +1,98 @@
+typealias Point = Pair<Int, Int>
+typealias Looping = Int
+
+enum class Direction() {
+    UP, RIGHT, DOWN, LEFT;
+
+    /*fun getChar(): Char {
+        return this.char
+    }*/
+
+    fun move(): Point {
+        return when (this) {
+            UP -> Point(0, -1)
+            RIGHT -> Point(1, 0)
+            DOWN -> Point(0, 1)
+            LEFT -> Point(-1, 0)
+        }
+    }
+
+    fun rotate(): Direction {
+        return when (this) {
+            UP -> RIGHT
+            RIGHT -> DOWN
+            DOWN -> LEFT
+            LEFT -> UP
+        }
+    }
+}
+
+class Grid(input: List<String>) {
+    private val grid: HashMap<Point, Char> = input.foldIndexed(hashMapOf<Point, Char>()) { y, grid, row ->
+        row.toCharArray().forEachIndexed { x, c ->
+            grid[Point(x, y)] = c
+        }
+        grid
+    }
+
+    operator fun get(point: Point) = grid[point]
+
+    operator fun set(point: Point, value: Char) {
+        grid[point] = value
+    }
+
+    fun find(char: Char): Point? = grid.entries.find { it.value == char }?.key
+    fun contains(point: Point) = grid.contains(point)
+}
+
+fun main() {
+    fun add(point: Point, direction: Point): Point {
+        return Pair(point.first + direction.first, point.second + direction.second)
+    }
+
+    /**
+     * Returns a tuple of ( visited points, Looping bit )
+     * Looping == 1 => Entered a loop
+     */
+    fun walk(grid: Grid): Pair<Set<Point>, Looping> {
+        var direction = Direction.UP
+        var guard = grid.find('^')!!
+        val visited: HashSet<Pair<Point, Direction>> = hashSetOf()
+        while (visited.add(Pair(guard, direction)) /* returns false if present */) {
+            val next = add(guard, direction.move())
+            when (grid[next]) {
+                '#' -> direction = direction.rotate()
+                is Char -> guard = next
+                /** no return -> found the exit */
+                else -> return Pair(visited.map { it.first }.toSet(), 0)
+            }
+        }
+
+        return Pair(visited.map { it.first }.toSet(), 1)
+    }
+
+    fun part1(input: List<String>): Int {
+        return walk(Grid(input)).first.size
+    }
+
+    fun part2(input: List<String>): Int {
+
+        return walk(Grid(input)).first.sumOf { point ->
+            /** For each walked point (except the first), replace it with a wall and walk it */
+            Grid(input).apply {
+                if (this[point] != '^') this[point] = '#'
+            }.let {
+                walk(it).second
+            }
+        }
+    }
+
+// Or read a large test input from the `src/Day01_test.txt` file:
+    val testInput = readInput("Day06_test")
+    check(part1(testInput) == 41)
+    check(part2(testInput) == 6)
+// Read the input from the `src/Day01.txt` file.
+    val input = readInput("Day06")
+    part1(input).println()
+    part2(input).println()
+}
